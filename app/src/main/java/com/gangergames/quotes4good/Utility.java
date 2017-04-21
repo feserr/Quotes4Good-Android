a0e7abe37e73ba6e96aedde14e8ab736ffe91ad3
feserr <fraserm1989@gmail.com>
2017-04-21 13:56:33 +0200

    Add a file downlader task to check the version.

--- app/src/main/java/com/gangergames/quotes4good/Utility.java
+++ app/src/main/java/com/gangergames/quotes4good/Utility.java
@@ -182,16 +182,4 @@ public class Utility {
 
         return ret;
     }
-
-    /**
-     * @param context used to check the device version and DownloadManager information
-     * @return true if the download manager is available
-     */
-    public static boolean isDownloadManagerAvailable(Context context) {
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
-            return true;
-        }
-        return false;
-    }
 }
